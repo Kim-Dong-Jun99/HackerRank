3e1c773fb8766636037df6095008b518a0fe17af
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2021-12-13 11:44:09 +0900

    정수 어레이를 구성하는 수 중에 가장 빈도 수가 높은 수 찾기

---
+++ Algorithm/Implementation/Migratory Birds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    setBirds = set(arr)
+    max = 0
+    maxBird = 0
+    for i in setBirds:
+        if arr.count(i) > max:
+            maxBird = i
+            max = arr.count(i)
+
+    return maxBird
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
