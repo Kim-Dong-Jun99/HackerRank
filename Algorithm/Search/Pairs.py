d9aaf156f9a9bb60a599c4c7b529bc6c5286bc29
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2021-12-18 13:12:42 +0900

    같은 차이가 나는 쌍의 최대 개수 구하기

---
+++ Algorithm/Search/Pairs.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr):
+    # Write your code here
+    arr.sort()
+    queue = []
+    result = 0
+    for i in arr:
+        if queue == []:
+            queue.append(i+k)
+        else:
+            if queue.count(i) != 0:
+                result += 1
+                queue = queue[queue.index(i)+1:]
+            queue.append(i+k)
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
